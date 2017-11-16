951f8897d8953616d256aa9a4c3c310cd625b505
Berk ÇEKİŞBAŞ <berkcekisbas@hotmail.com>
2017-11-16 11:35:09 +0300

    fosuser bundle included

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class DefaultController extends Controller
 {
@@ -19,9 +20,21 @@ class DefaultController extends Controller
         $yeterlilik = new Yeterlilik();
 
         $form = $this->createFormBuilder($yeterlilik)
-        ->add('ad',TextType::class)
-        ->add('save', SubmitType::class, array('label' => 'Kaydet'))
-        ->getForm();
+            ->add('ad',TextType::class, array('attr'  => array('class' => 'form-control')))
+            ->add('kod',TextType::class, array('attr'  => array('class' => 'form-control')))
+            ->add('sektor', EntityType::class, array(
+                'attr'  => array('class' => 'form-control'),
+            'placeholder' => 'Sektör Seçiniz',
+            'required' => true,
+            'class' => 'AppBundle:Sektor',
+             // use the User.username property as the visible option string
+             'choice_label' => 'ad',
+             // used to render a select box, check boxes or radios
+             // 'multiple' => true,
+             // 'expanded' => true,
+         ))
+            ->add('save', SubmitType::class, array('label' => 'Kaydet','attr'  => array('class' => 'btn green')))
+            ->getForm();
 
         return $this->render('AppBundle::test.html.twig', array(
             'form' => $form->createView(),
